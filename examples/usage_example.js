8a9cdc8844e6b12b2d7849c7e58cb55bad533a65
Gabriel Demetrios Lafis <33261275+galafis@users.noreply.github.com>
2026-02-19 16:34:31 -0300

    Aprimorar qualidade do projeto: exemplos de uso

---
+++ examples/usage_example.js
@@ -0,0 +1,17 @@
+/**
+ * Exemplo de uso - Criando-um-Gerador-de-QR-Codes-para-E-commerces-com-Node.js
+ *
+ * Demonstra as principais funcionalidades do projeto.
+ */
+
+// Importar modulos principais
+// const { generateQRCode } = require('./src/index.js');
+
+async function main() {
+  // Inicializar generateQRCode
+  // const instance = new generateQRCode();
+  console.log("generateQRCode carregado com sucesso");
+
+}
+
+main().catch(console.error);
